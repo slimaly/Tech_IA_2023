8aa90ac3abf8acbe672e0ee464d2209abace2c3d
Alycia Slimani <alyciaslimani@yahoo.fr>
2023-12-19 13:13:42 +0100

    updates

---
+++ exercice_package/parent_file.py
@@ -0,0 +1,5 @@
+def multiply (a,b):
+    return a * b
+
+
+print(multiply(a= 8, b= 17))
---
+++ exercice_package/principal/main.py
@@ -0,0 +1,7 @@
+from slevel import add
+from parent_file import multiply
+from child_folder.child import divide
+
+
+result = (5 + multiply(2, 3)) / 3
+rounded_result = round(result, 2)
